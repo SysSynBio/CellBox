9869e9fac80daa806c4432c1e28610a9ab8555d8
Song Feng <song.feng@outlook.com>
2023-03-09 22:18:20 +0000

    update

---
+++ cyano_rna/cyano_rna_slurm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -A hyperbio
+#SBATCH -t 4-0
+#SBATCH -N 1
+#SBATCH -n 32
+#SBATCH -o cellbox_out
+#SBATCH -e cellbox_err
+#SBATCH --mail-user=song.feng@pnnl.gov
+#SBATCH --mail-type END
+
+module purge
+module load python/miniconda22.11
+
+conda activate cellbox
+
+export PATH=/people/feng626/.conda/envs/cellbox/bin:$PATH
+
+cd /people/feng626/phenome/SystemModeling/CellBox
+
+python scripts/main.py -config=cyano_rna/cyano_rna_config.json -i=230309
