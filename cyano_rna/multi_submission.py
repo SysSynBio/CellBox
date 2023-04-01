40d3c26cbb0b5d5591b9ecea8da889a817873d35
Song Feng <song.feng@outlook.com>
2023-03-31 17:01:08 -0700

    update

--- cyano_rna/multi_submission.py
+++ cyano_rna/multi_submission.py
@@ -3,7 +3,8 @@
 import os, shutil, sys
 
 name = "cyano_rna_slurm"
-dispatches = list(range(20))
+dispatch_num = 20
+loop_num = 50
 
 account = "hyperbio"
 time = "4-0" 
@@ -17,20 +18,23 @@ extras = [
         "source /share/apps/python/miniconda-py310_22.11.1-1/etc/profile.d/conda.sh", 
         "conda activate cellbox",
         "export PATH=/people/feng626/.conda/envs/cellbox/bin:$PATH",
-        "cd /people/feng626/phenome/SystemModeling/CellBox",
         ]
+workdir = "cd /people/feng626/phenome/SystemModeling/CellBox"
 
+slurm_template = "#!/bin/sh\n#SBATCH -A " + account + "\n#SBATCH -t " + time + "\n#SBATCH -N " + str(node) + "\n#SBATCH -n " + str(core)
 
-slurm = "#!/bin/sh\n#SBATCH -A " + account + "\n#SBATCH -t " + time + "\n#SBATCH -N " + str(node) + "\n#SBATCH -n " + str(core)
-
-for dispatch in dispatches:
+for dispatch in range(dispatch_num):
+    slurm = slurm_template
     slurm += "\n#SBATCH -o " + out + '_' + str(dispatch) + ".txt\n#SBATCH -e " + err + '_' + str(dispatch) + ".txt\n#SBATCH --mail-user=" + mail + "\n#SBATCH --mail-type " + "END" + "\n \nmodule purge\n"
     for module in modules:
-        slurm += "module load " + module + '\n \n'
+        slurm += "module load " + module + '\n'
+    slurm += '\n'
     for extra in extras:
-        slurm += extra + '\n \n'
+        slurm += extra + '\n'
+    slurm += '\n'
+    slurm += workdir + "\n \n"
 
-    slurm += "max = 50\nfor i in `seq 0 $max`\ndo\n    python scripts/main.py -config=cyano_rna/cyano_rna_config.json -i=$i\ndone\n"
+    slurm += "max = " + str((dispatch + 1) * loop_num - 1) + "\nfor i in `seq " + str(dispatch * loop_num) + " $max`\ndo\n    python scripts/main.py -config=cyano_rna/cyano_rna_config.json -i=$i\ndone\n"
 
     filename = name + "_rep" + str(dispatch) + ".sbatch"
     with open(filename, 'w') as sbatch:
